e3afef784cc2c262983bfbae4e1daa0bf3b0cb9f
aman1210 <aman.srivastava101@gmail.com>
2020-10-13 18:38:50 +0530

    Profile Page Challenge

---
+++ Profile/android/app/src/main/kotlin/com/example/uiChallengeProfile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uiChallengeProfile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Profile/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:line_awesome_icons/line_awesome_icons.dart';
+import 'package:uiChallengeProfile/profileHeader.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile UI',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ProfileHeader(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(LineAwesomeIcons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(LineAwesomeIcons.bookmark), label: "Saved"),
+          BottomNavigationBarItem(
+              icon: Icon(LineAwesomeIcons.thumbs_up), label: "Liked"),
+          BottomNavigationBarItem(
+              icon: Icon(LineAwesomeIcons.user), label: "Profile"),
+        ],
+        currentIndex: 3,
+        elevation: 10,
+        onTap: (value) {},
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 28.0,
+      ),
+    );
+  }
+}
