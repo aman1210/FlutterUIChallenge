25fed68cb8467a331efd29217cea300d91f1d768
aman1210 <aman.srivastava101@gmail.com>
2021-01-04 16:25:26 +0530

    background change working

--- Travel App/lib/Screens/homepage.dart
+++ Travel App/lib/Screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:travel_app/widgets/background.dart';
@@ -11,58 +12,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentImage = 'assets/images/varanasi1.jpg';
+
+  List<String> images = [
+    'assets/images/varanasi1.jpg',
+    'assets/images/varanasi2.jpg',
+    'assets/images/varanasi3.jpg',
+    'assets/images/varanasi4.jpg',
+    'assets/images/varanasi5.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
-    var currentImage = 'assets/images/varanasi1.jpg';
-
-    List<String> images = [
-      'assets/images/varanasi1.jpg',
-      'assets/images/varanasi2.jpg',
-      'assets/images/varanasi3.jpg',
-      'assets/images/varanasi4.jpg',
-      'assets/images/varanasi5.jpg',
-    ];
-
     return Scaffold(
       body: Stack(
         children: [
-          BackgroundImages(size: size, image: currentImage),
-          Center(
-            child: CarouselSlider.builder(
-              carouselController: ScrollController(),
-              options: CarouselOptions(
-                aspectRatio: 9 / 16,
-                height: 500,
-                viewportFraction: 0.85,
-                pageSnapping: true,
-                enlargeCenterPage: true,
+          BackgroundImages(
+            image: currentImage,
+            size: size,
+          ),
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 60, left: 20, right: 20),
+                height: 65,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    Column(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/icon.png',
+                          height: 30,
+                        ),
+                        Text(
+                          'travello',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontFamily: 'Poppins'),
+                        )
+                      ],
+                    ),
+                    Icon(
+                      CupertinoIcons.search,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
               ),
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: size.width,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                          images[index],
+              CarouselSlider.builder(
+                options: CarouselOptions(
+                  aspectRatio: 9 / 16,
+                  height: 500,
+                  viewportFraction: 0.8,
+                  pageSnapping: true,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentImage = images[index];
+                    });
+                  },
+                ),
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: size.width,
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            images[index],
+                          ),
                         ),
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black38,
-                          offset: Offset(4, 4),
-                        ),
-                      ]),
-                );
-              },
-            ),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black38,
+                            offset: Offset(4, 4),
+                          ),
+                        ]),
+                  );
+                },
+              ),
+            ],
           ),
         ],
       ),
--- Travel App/lib/widgets/background.dart
+++ Travel App/lib/widgets/background.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class BackgroundImages extends StatelessWidget {
+class BackgroundImages extends StatefulWidget {
   const BackgroundImages({
     Key key,
     @required this.size,
@@ -13,23 +13,46 @@ class BackgroundImages extends StatelessWidget {
   final String image;
 
   @override
+  _BackgroundImagesState createState() => _BackgroundImagesState();
+}
+
+class _BackgroundImagesState extends State<BackgroundImages>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    animation = CurvedAnimation(curve: Curves.easeIn, parent: controller);
+    controller.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: size.height,
-      width: size.width,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            image,
+    return FadeTransition(
+      opacity: animation,
+      child: Container(
+        height: widget.size.height,
+        width: widget.size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              widget.image,
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
         ),
-      ),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.0),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.3),
+            ),
           ),
         ),
       ),
