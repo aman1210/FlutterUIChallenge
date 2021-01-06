640832404a3aa97326aa08a170953b2603d58bff
aman1210 <aman.srivastava101@gmail.com>
2021-01-06 10:44:40 +0530

    hovering animation added

--- BalloonAnimation/lib/homepage.dart
+++ BalloonAnimation/lib/homepage.dart
@@ -5,48 +5,78 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   AnimationController controller;
+  AnimationController controller2;
   Animation<double> slideAnimation;
-  Animation<double> hoverAnimation;
+  Animation<double> sizeAnimation;
+
+  double balloonbottomlocation;
 
   @override
   void initState() {
     super.initState();
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 2));
-    slideAnimation = Tween<double>(begin: 0, end: 300).animate(controller)
+    controller2 =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    balloonbottomlocation = MediaQuery.of(context).size.height - 200;
+    slideAnimation =
+        Tween<double>(begin: balloonbottomlocation, end: 0).animate(
+      controller,
+    )..addListener(() {
+            setState(() {});
+          });
+    sizeAnimation = Tween<double>(begin: 50, end: 200).animate(controller)
       ..addListener(() {
         setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (controller.status == AnimationStatus.completed) {
+          print('hello');
+          slideAnimation = Tween<double>(begin: 0, end: 50).animate(controller2)
+            ..addListener(() {
+              setState(() {});
+            })
+            ..addStatusListener((status) {
+              if (status == AnimationStatus.completed) {
+                controller2.reverse();
+              } else if (status == AnimationStatus.dismissed) {
+                controller2.forward();
+              }
+            });
+          controller2.forward();
+        }
       });
-
     controller.forward();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // print(controller.value);
     return Scaffold(
-      body: Stack(children: [
+        body: Stack(
+      overflow: Overflow.visible,
+      children: [
         Positioned(
-          bottom: slideAnimation.status == AnimationStatus.completed
-              ? hoverAnimation.value
-              : slideAnimation.value,
+          top: slideAnimation.value,
           child: Container(
-            height: slideAnimation.value,
-            width: slideAnimation.value,
+            height: sizeAnimation.value,
+            width: sizeAnimation.value,
             color: Colors.blue,
           ),
-        ),
-      ]),
-    );
+        )
+      ],
+    ));
   }
 }
