676c187299071e27fb23fe5bf9c4022e0c6001df
aman1210 <aman.srivastava101@gmail.com>
2021-01-06 09:27:50 +0530

    starting balloonAnimation

---
+++ BalloonAnimation/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:BalloonAnimation/homepage.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
